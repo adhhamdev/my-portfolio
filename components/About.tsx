5529b863f0b0df67828120ca66ecff4de6674598
Adhham Safwan <msmadhhamg@gmail.com>
2024-11-03 15:59:24 +0530

    Update About component content

--- components/About.tsx
+++ components/About.tsx
@@ -7,16 +7,16 @@ export default function About() {
         <h2 className='text-3xl font-bold mb-8 text-center'>About Me</h2>
         <div className='max-w-2xl mx-auto'>
           <p className='mb-4'>
-            Highly motivated BTEC HND in Software Engineering student and
-            self-taught Full Stack Developer with a strong focus on buildlling
-            websites with exceptional SEO and accessibility. Skilled in creating
-            visually attractive and high-performance websites. Seeking
-            opportunities to contribute my expertise and creativity. I have a
-            passion for learning new technologies and building beautiful and
-            functional websites. I am a firm believer in the importance of good
-            design and user experience. I am always looking for new challenges
-            and opportunities to grow. I am currently looking for a challenging
-            and exciting role in a fast-paced and growing company.
+            Highly motivated Undergraduate student and self-taught Full Stack
+            Developer with a strong focus on building websites with exceptional
+            SEO and accessibility. Skilled in creating visually attractive and
+            high-performance websites. Seeking opportunities to contribute my
+            expertise and creativity. I have a passion for learning new
+            technologies and building beautiful and functional websites. I am a
+            firm believer in the importance of good design and user experience.
+            I am always looking for new challenges and opportunities to grow. I
+            am currently looking for a challenging and exciting role in a fast-
+            paced and growing company.
           </p>
           <p>
             I thrive in collaborative environments and am always eager to take
